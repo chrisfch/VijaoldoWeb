54c626871e21077446b5fa776c3dbf99cdcbd0a6
CHRISTIAN <christian_fa_1@hotmail.com>
2025-09-17 18:30:33 -0500

    Creando la base 1.0

--- AdministracionApp/urls.py
+++ AdministracionApp/urls.py
@@ -1,6 +1,8 @@
-from .views import inicio
+from .views import paginaIndex, ventas, iniciarsesion
 from django.urls import path
 
 urlpatterns = [
-    path('', inicio, name='inicio'),
+    path('', paginaIndex, name='inicio'),
+    path('iniciarsesion', iniciarsesion, name='iniciarsesion'),
+    path('ventas', ventas, name='ventas'),
 ]
--- AdministracionApp/views.py
+++ AdministracionApp/views.py
@@ -2,10 +2,28 @@ from django.shortcuts import render
 from django.db import connection
 from datetime import datetime
 
-def inicio(request):
+def paginaIndex(request):
+    return render(request, 'index.html')
+
+def iniciarsesion(request):
+    if request.method == 'POST':
+        #try:
+            with connection.cursor() as cursor:
+                cursor.execute("EXEC JO_sp_ListarTrabajador %s, %s, %s",[
+                    2,
+                    request.POST['usuario'],
+                    request.POST['password']
+                ])
+                filas = cursor.fetchall()
+                if filas:
+                    return render(request, 'ventas.html', {'filas': filas})
+                else:
+                    return render(request, 'login.html', {'error': 'Usuario o contraseña incorrectos'})
+    return render(request, 'login.html')
+
+def ventas(request):
     with connection.cursor() as cursor:
         cursor.execute("EXEC JO_sp_ListarVenta %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s", 
-                       [1, 0, 0, 0, datetime.now(), datetime.now(), 0, 0, 'TIENDA VIRREY 1', '', 0, 0, ''])  # Reemplaza 'SomeTable' con el nombre real de tu tabla
+                       [1, 0, 0, 0, datetime.now(), datetime.now(), 0, 0, 'TIENDA VIRREY 2', '', 0, 0, ''])  # Reemplaza 'SomeTable' con el nombre real de tu tabla
         rows = cursor.fetchall()
-    return render(request, 'index.html',{'rows': rows})
-
+    return render(request, 'ventas.html',{'rows': rows})
